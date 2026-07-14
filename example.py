b8681b91bf61e523d6a9fe78bd02baec6e9a3f3f
tgcabuang <thristiancabuang27@gmail.com>
2026-07-14 09:22:04 +0000

    Try

--- example.py
+++ example.py
@@ -2,7 +2,7 @@
 
 def git_opeation():
 
- print("I am adding example.py file to the remote repository.")
+ print("TryI am adding example.py file to the remote repository.")
 
 git_opeation()
 =======
