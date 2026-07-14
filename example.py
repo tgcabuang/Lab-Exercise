945ae9c82194e13af1f497187081752a7313c38c
Thristian Cabuang <119090932+tgcabuang@users.noreply.github.com>
2026-07-14 17:19:05 +0800

    Delete example.py

--- example.py
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-def git_opeation():
-
- print("IHays am adding example.py file to the remote repository, Ok.")
-
-git_opeation()
-=======
-
->>>>>>> bb85e4d5cb17005faf830aa5d82ef12bc3a489fb
